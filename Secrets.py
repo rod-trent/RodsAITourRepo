d2ccf9e3bd3330c613b57569420e1e91120b4ce4
Rod Trent <56966432+rod-trent@users.noreply.github.com>
2024-01-05 15:18:40 -0500

    Create Secrets.py

---
+++ Secrets.py
@@ -0,0 +1,18 @@
+Fake_social_security_number = "888-88-888888"
+
+Fake_credit_card_number = "5555-5555-5555-5555"
+
+Fake_age = "151"
+
+Fake_mothers_maiden_name = "Juniper"
+
+Fake_drivers_license_number = "RG00000"
+
+Fake_car_license_number = "B1G EG0"
+
+print Fake_social_security_number
+print Fake_credit_card_number
+print Fake_age
+print Fake_mothers_maiden_name
+print Fake_drivers_license_number
+print Fake_car_license_number
